2d5c398c9b45275f58958b9c2080011637799410
ybl <1162474984@qq.com>
2019-12-23 15:53:35 +0800

    新增get、post可以发送数据

--- src/static/tool.js
+++ src/static/tool.js
@@ -8,36 +8,46 @@ if (window.XMLHttpRequest) {
     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
 
+//参数处理
+function paramResult(data) {
+    var param = "";
+    var count = 0;
+    for (var x in data) {
+        var paramO = x + "=" + data[x];
+        if (count > 0) {
+            param += "&" + paramO
+        } else {
+            param += paramO;
+        }
+        count++;
+    }
+    return param;
+}
+//get请求方式
 function get_request(url, data) {
-    xmlhttp.open('GET', PREFIX_REQUEST + url);
+    var param = "?" + paramResult(data);
+    xmlhttp.open('GET', PREFIX_REQUEST + url + param);
+    var token = window.localStorage.token ? window.localStorage.token : '';
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.setRequestHeader("authorization", window.localStorage.token);
+    xmlhttp.setRequestHeader("authorization", token);
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             console.log(xmlhttp.responseText);
         }
     };
-
-    xmlhttp.send(data);
+    xmlhttp.send();
 }
-
-// function request(url, type, data) {
-//     var xmlhttp;
-//     if (window.XMLHttpRequest) {
-//         //  IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-//         xmlhttp = new XMLHttpRequest();
-//     } else {
-//         // IE6, IE5 浏览器执行代码
-//         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-//     }
-//     xmlhttp.open(type, PREFIX_REQUEST+url);
-//
-//     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-//     xmlhttp.onreadystatechange = function () {
-//         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-//             console.log(xmlhttp.responseText);
-//         }
-//     }
-//     xmlhttp.send('msg=大萨达打');
-// }
+//post请求方式
+function post_request(url, data) {
+    xmlhttp.open('POST', PREFIX_REQUEST + url);
+    var token = window.localStorage.token ? window.localStorage.token : '';
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.setRequestHeader("authorization", token);
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            console.log(xmlhttp.responseText);
+        }
+    };
+    xmlhttp.send(paramResult(data));
+}
 
